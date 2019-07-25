b949873f5e4ac8c6ea51e900e7874f7bc0109e9c
sharpevb <victoriabsharpe@gmail.com>
2019-07-25 14:26:33 -0400

    omdb, spotify, and bandsintown working! reformatting data more readable

--- liri.js
+++ liri.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var keys = require("./keys.js");
-var moment = require("moment")
+var moment = require("moment");
+var m = moment();
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
@@ -27,29 +28,37 @@ function input(command, title) {
             doThis(title);
             break;
         default:
-            console.log("Please enter one of the following commands: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+            console.log("\n");
+            console.log("-------------------------------------------");
+            console.log("Please enter one of the following commands")
+            console.log("-------------------------------------------");
+            console.log("concert-this \nspotify-this-song \nmovie-this \ndo-what-it-says \n")
     }
 }
 console.log(input(command, title));
 
 
 // Concert-This command call
-// Needs some work
+// Needs Moment.js 
 function concertThis(title) {
+    if (!title) {
+        console.log("Please type a band or artist")
+    }
     var queryURl = "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp"
     axios.get(queryURl).then(function (response) {
-    var concertList = response.data    
+        var concertList = response.data
+
+        //Trying to get the datetime formatted using Moment 
+        //m = moment.utc("2019-07-25T18:30:00, MM/DD/YYYY HH:mm:ss");   
 
         for (var i = 0; i < concertList.length; i++) {
-            //Trying to get the datetime formatted using Moment 
-                //var date = moment(datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")
             console.log("--------------------------------");
             console.log("Search Results:");
             console.log("--------------------------------");
-            console.log("  Venue: " + concertList[i].venue.name);            
+            console.log("  Venue: " + concertList[i].venue.name);
             console.log("  Country: " + concertList[i].venue.country);
             console.log("  State: " + concertList[i].venue.region);
-            console.log("  City: " + concertList[i].venue.city);            
+            console.log("  City: " + concertList[i].venue.city);
             console.log("  Date: " + concertList[i].datetime)
             console.log("\n")
         }
@@ -58,41 +67,28 @@ function concertThis(title) {
 
 
 
-//Movie-This OMDB 
+//Movie-This OMDB !!!!!
 function movieThis(title) {
     if (!title) {
         title = "Mr. Nobody"
     }
     var queryURl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy"
-
-
     axios.get(queryURl).then(function (response) {
+        console.log("--------------------------------");
+        console.log("Search Results:");
+        console.log("--------------------------------");
+        console.log("  Title: " + response.data.Title);
+        console.log("  Released: " + response.data.Released);
+        console.log("  IMDb Rating: " + response.data.imdbRating)
+        console.log("  Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("  Produced in: " + response.data.Country);
+        console.log("  Language: " + response.data.Language);
+        console.log("  Plot: " + response.data.Plot);
+        console.log("  Actors: " + response.data.Actors);
+        console.log("\n");
 
     });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Spotify-This-Song!!!!!
