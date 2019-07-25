ffc1220e7e03bc0c6848fdc29917aa5b8a2b6c42
sharpevb <victoriabsharpe@gmail.com>
2019-07-25 00:40:50 -0400

    start of a working concert-this

--- liri.js
+++ liri.js
@@ -25,52 +25,102 @@ function input(command, title) {
         case "do-what-it-says":
             doThis(title);
             break;
-    default:
-        console.log("Please enter one of the following commands: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")         
+        default:
+            console.log("Please enter one of the following commands: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
     }
 }
 console.log(input(command, title));
 
-function spotifyThis(title) {
-    //if (!title) {
-       // title = "The Sign"
-    //}
-    spotify.search({ type: 'track', query: title }, function (err, data) {
-    if (err) {
-        throw "There was an error: " + err;
+
+// Concert-This command call
+// Needs some work
+function concertThis(title) {
+    var queryURl = "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp"
+    axios.get(queryURl).then(function(response) {
+        var concerts = response.data;
+        console.log(concerts);
+    })
+}
+
+
+
+//Movie-This OMDB 
+function movieThis(title) {
+    if (!title) {
+        title = "Mr. Nobody"
     }
-var musicData = data.tracks.items[0];
-console.log("--------------------------------")
-console.log("Search Results:")
-console.log("--------------------------------")
-console.log("  Artist Name: " + musicData.artists[0].name + "\n  Song Title: " + musicData.name +
-"\n  Album Title: " + musicData.album.name +
-"\n  Preview URL: " + musicData.preview_url);
-console.log("--------------------------------")
-});
+    var queryURl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy"
+
+
+    axios.get(queryURl).then(function (response) {
+
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Spotify-This-Song!!!!!
+function spotifyThis(title) {
+    if (!title) {
+        title = "Ace of Base"
+    }
+    spotify.search({ type: 'track', query: title }, function (err, data) {
+        if (err) {
+            throw "There was an error: " + err;
+        }
+        var musicData = data.tracks.items[0];
+        console.log("--------------------------------")
+        console.log("Search Results:")
+        console.log("--------------------------------")
+        console.log("  Artist Name: " + musicData.artists[0].name + "\n  Song Title: " + musicData.name +
+            "\n  Album Title: " + musicData.album.name +
+            "\n  Preview URL: " + musicData.preview_url);
+        console.log("--------------------------------")
+    });
 };
 
 
 
 /*
-if (command === "concert-this") {
-    console.log("concert");
-};
-
-if (title === "")
-
-
 
 CODE GRAVEYARD
-
 
 axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
   function(response) {
     console.log("The movie's rating is: " + response.data.imdbRating);
   }
+
 );
 
 //var bands = new Bands(keys.bands);
 //var omdb = new (keys.omdb);
+
+if (command === "concert-this") {
+    console.log("concert");
+};
+
+if (title === "")
 
 */
